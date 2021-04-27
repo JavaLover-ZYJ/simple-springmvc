3ff6b9270a68927eaf5c5b68c8405cce03256655
zhouyuejia <zhouyuejia@yxqiche.com>
2021-04-27 16:23:10 +0800

    初始化代码

---
+++ src/main/java/com/zyj/service/UserService.java
@@ -0,0 +1,13 @@
+package com.zyj.service;
+
+import com.zyj.bean.User;
+
+/**
+ * @author zyj
+ */
+public interface UserService {
+
+     public  void  findUser();
+
+     public User getUser();
+}
